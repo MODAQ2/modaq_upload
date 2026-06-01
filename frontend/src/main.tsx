1c9edd8c8f7943e60d4d97683032782ffe39843b
Simms, Andrew <andrew.simms@nlr.gov>
2026-06-01 07:03:31 -1000

    Frontend: Wire up global error reporting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { installGlobalErrorReporting } from './utils/errorReporter.ts';
+
+// Capture uncaught browser errors / promise rejections and ship them to S3.
+installGlobalErrorReporting();
 
 // biome-ignore lint/style/noNonNullAssertion: root element is guaranteed to exist in index.html
 createRoot(document.getElementById('root')!).render(
